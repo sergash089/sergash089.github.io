39d2485159459d9436602cb2b5af597495f6bc4b
sergash089 <sergash.089@gmail.com>
2026-06-14 09:35:43 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,11 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
-
 import tailwindcss from '@tailwindcss/vite';
 import icon from 'astro-icon';
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://sergash089.github.io',
-  base: '/elite-phone/',
+  base: '/sergash089/',
   integrations: [react(), icon()],
 
   vite: {
